b5da79dbb5003bd89f07e24a3bccc9f5083b8bc9
Camilo Arthur <camilo.arthur@gmail.com>
2010-10-19 19:47:12 -0200

    Projeto Post DevRS.Net "Introdução a Testes Unitários utilizando Visual Studio 2010"
    Signed-off-by: Camilo Arthur <camilo.arthur@gmail.com>

---
+++ Calculadora/Calculadora/Operacoes.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Calculadora
+{
+    public static class Operacoes
+    {
+        public static long Somar(long numero1, long numero2)
+        {
+            return numero1 + numero2;
+        }
+
+        public static long Subtrair(long minuendo, long subtraendo)
+        {
+            return minuendo - subtraendo; 
+        }
+
+        public static long Multiplicar(long multiplicando, long multiplicador)
+        {
+            return multiplicando * multiplicador;
+        }
+
+        public static double Dividir(double dividendo, double divisor)
+        {
+            try
+            {
+                return dividendo / divisor;
+            }
+            catch (System.DivideByZeroException)
+            {
+                throw new System.DivideByZeroException("O divisor não pode ser zero!");
+            }
+        }
+    }
+}
